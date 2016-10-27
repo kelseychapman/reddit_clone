b63b872ca2ef07f13ce9a3b288350d164c7dd19e
Kelsey Chapman <kelseyc.chapman@gmail.com>
2016-10-27 10:44:06 -0600

    added colorpicker

--- script.js
+++ script.js
@@ -77,6 +77,33 @@ $scope.toggleForm = function (){
 	    }
 	  }
 
+//colorpicker
+		angular.module('minicolors-angular', [])
+		  .directive('minicolors', function () {
+		    return {
+		      require: 'ngModel',
+		      priority: 1,
+		      scope: {
+		        ngModel: '='
+		      },
+		      link: function postLink(scope, element, attrs, ngModel) {
+		        var options;
+		        try {
+		          options = scope.$eval(attrs.minicolors);
+		        } catch(err) {
+		          console.error("invalid option object");
+		          options = null;
+		        }
+		        element.minicolors(options);
+
+		        ngModel.$render = function () {
+		          element.minicolors('value', ngModel.$viewValue);
+		        };
+		      }
+		    };
+		  });
+
+
 //seeding post info
 	$scope.posts = [
 	   {
